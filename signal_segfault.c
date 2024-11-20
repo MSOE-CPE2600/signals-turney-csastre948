5900a25ca002a28ad013a939319d5d763a08526f
Carla Sastre <sastrec@msoe.edu>
2024-11-20 11:25:41 -0600

    Added SIGSEGV handler to terminate gracefully and updated comments

--- signal_segfault.c
+++ signal_segfault.c
@@ -1,24 +1,33 @@
 /**
  * @file signal_segfault.c
- * @brief Program that dereferences a null pointer causing a segmentation fault
+ * @brief Handles SIGSEGV caused by dereferencing a NULL pointer.
+ *
+ * Modified by: Carla Sastre
  */
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * Modified by:
- * 
- * Brief summary of modifications:
+ * @brief Signal handler for SIGSEGV - prints a message when segmentation fault occurs.
  */
+void handle_segfault(int sig) {
+    printf("Caught SIGSEGV: Segmentation fault occurred.\n");
+    exit(1); // Terminate the program
+}
 
+int main() {
+    // Register the signal handler for SIGSEGV
+    signal(SIGSEGV, handle_segfault);
 
-#include <stdio.h>
+    // Dereference a NULL pointer to cause a segmentation fault
+    printf("Dereferencing a NULL pointer to cause SIGSEGV...\n");
+    int *ptr = NULL;
+    printf("The value at ptr is: %d\n", *ptr); // This line causes SIGSEGV
 
-int main (int argc, char* argv[]) {
-    // Declare a null pointer
-    int* i = NULL;
+    // Program execution will return here after handling SIGSEGV
+    printf("Continuing after SIGSEGV...\n");
 
-    // Dereference the null pointer
-    printf("The value of i is: %d\n", *i);
-
-    // Return to exit the program
     return 0;
 }
